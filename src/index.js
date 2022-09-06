847ca89b82336384d98d042496ef1ae016d3ee64
Anastasia Slezko <slezkoanastasiia@gmail.com>
2022-09-06 16:18:30 +0300

    api/react/movies
    2 sample/update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from 'components/App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-05-movies/">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
